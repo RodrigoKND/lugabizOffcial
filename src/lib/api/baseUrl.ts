ca9f94aa99c69d67e8c75966e6154bad2ff41f72
RodrigoKND <84539101+RodrigoKND@users.noreply.github.com>
2025-07-30 13:44:41 -0400

    Removing mode production

--- src/lib/api/baseUrl.ts
+++ src/lib/api/baseUrl.ts
@@ -1,3 +1,1 @@
-const isProduction = import.meta.env.MODE;
-
-export const baseUrl = isProduction === "Production" ? "https://lugabiz-api.vercel.app" : "http://localhost:5432";
+export const baseUrl = "https://lugabiz-api.vercel.app"
