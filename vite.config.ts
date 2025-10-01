4cc3c2fafa70f5971ab28ccb4395e817f04fc7a9
RodrigoKND <84539101+RodrigoKND@users.noreply.github.com>
2025-10-01 14:18:59 -0400

    Resolving alias path

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,4 +7,14 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
   base: '/',
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@components': '/src/components',
+      '@context': '/src/context',
+      '@icons': '/src/icons',
+      '@pages': '/src/pages',
+      '@utils': '/src/utils',
+    },
+  },
 });
